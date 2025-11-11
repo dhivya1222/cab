54904e866e1e4016fc8cd13d6d5a8a417373ef82
manoj009876540987654 <144827758+manoj009876540987654@users.noreply.github.com>
2025-11-11 09:27:06 +0530

    Change Cognito configuration in config.js
    Updated Cognito user pool IDs and client IDs.

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_xLe9mp7PL', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2qhnbpedvpsufeinhbt88suneo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_y578hBejS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '72gduv81m0jrfsmob4ppmgmbpf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ralmchxygd.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
