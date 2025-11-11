dcc529b4c8ae4cdd08d9ab13e5814cf8ec475b32
dhivya1222 <dhivyanandhagopal12@gmail.com>
2025-11-11 15:46:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_GJZrZz31u', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ssks7k4t5t97e2cnmaiq55pa4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_e155AWAFQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3uok8v9enl1tcsj6ffppdu3hje', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://huf30pzrpb.execute-api.ap-south-1.amazonaws.com/prods' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
