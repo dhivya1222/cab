2fa4324e6265f88cb1b63e22722d198abf8b5a5b
dhivya1222 <dhivyanandhagopal12@gmail.com>
2025-11-11 16:19:58 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,10 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://2hgpj8q0j1.execute-api.ap-south-1.amazonaws.com/stage' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://2hgpj8q0j1.execute-api.ap-south-1.amazonaws.com/stage1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
